8db9fe4867e56dd051d2de49093b51ebc5a3f5ec
Sumita Melannoor <sumi.melannoor@gmail.com>
2026-08-10 10:34:25 +0200

    Add API base URL configuration

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,13 +1,14 @@
 import requests
+from utilities.config import BASE_URL
 def test_get_products():
-    response = requests.get('https://dummyjson.com/products')
+    response = requests.get(f'{BASE_URL}/products')
     data = response.json()
     assert response.status_code == 200
     assert 'products' in data
     assert type(data['products']) == list
 
 def test_single_product():
-    response = requests.get('https://dummyjson.com/products/1')
+    response = requests.get(f'{BASE_URL}/products/1')
     data = response.json()
     assert response.status_code == 200
     assert 'id' in data
@@ -15,21 +16,21 @@ def test_single_product():
     assert 'title' in data
 
 def test_add_product():
-    response = requests.post('https://dummyjson.com/products/add', json = {'title': 'Test Product','price': 100})
+    response = requests.post(f'{BASE_URL}/products/add', json = {'title': 'Test Product','price': 100})
     data = response.json()
     assert response.status_code == 201
     assert 'id' in data
     assert data['title'] == 'Test Product'
 
 def test_update_product():
-    response = requests.put('https://dummyjson.com/products/1',json = {'title': 'Updated Test Product'})
+    response = requests.put(f'{BASE_URL}/products/1',json = {'title': 'Updated Test Product'})
     data = response.json()
     assert response.status_code == 200
     assert data['id'] == 1
     assert data['title'] == 'Updated Test Product'
 
 def test_delete_product():
-    response = requests.delete('https://dummyjson.com/products/1')
+    response = requests.delete(f'{BASE_URL}/products/1')
     data = response.json()
     assert response.status_code == 200
     assert 'id' in data
