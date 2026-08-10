c18febfee77a17bfa08fb926f27c9e853b0ae1c7
Sumita Melannoor <sumi.melannoor@gmail.com>
2026-08-10 15:31:51 +0200

    Improve API request and response logging

--- api/product_api.py
+++ api/product_api.py
@@ -5,17 +5,27 @@ from utilities.logger import get_logger
 logger = get_logger(__name__)
 class ProductAPI:
     def get_products(self):
-        logger.info('getting products')
-        return requests.get(f'{BASE_URL}/products')
+        logger.info('GET products')
+        response= requests.get(f'{BASE_URL}/products')
+        logger.info(f'Response status: {response.status_code}')
+        return response
     def get_product(self, product_id):
-        logger.info('getting product')
-        return requests.get(f'{BASE_URL}/products/{product_id}')
+        logger.info(f'GET /products/{product_id}')
+        response= requests.get(f'{BASE_URL}/products/{product_id}')
+        logger.info(f'Response status: {response.status_code}')
+        return response
     def add_product(self, product):
-        logger.info('adding product')
-        return requests.post(f'{BASE_URL}/products/add', json=product)
+        logger.info('POST /products/add')
+        response= requests.post(f'{BASE_URL}/products/add', json=product)
+        logger.info(f'Response status: {response.status_code}')
+        return response
     def update_product(self, product_id, product):
-        logger.info('updating product')
-        return requests.put(f'{BASE_URL}/products/{product_id}', json=product)
+        logger.info(f'PUT /products/{product_id}')
+        response= requests.put(f'{BASE_URL}/products/{product_id}', json=product)
+        logger.info(f'Response status: {response.status_code}')
+        return response
     def delete_product(self, product_id):
-        logger.info('deleting product')
-        return requests.delete(f'{BASE_URL}/products/{product_id}')
+        logger.info(f'DELETE /products/{product_id}')
+        response= requests.delete(f'{BASE_URL}/products/{product_id}')
+        logger.info(f'Response status: {response.status_code}')
+        return response
